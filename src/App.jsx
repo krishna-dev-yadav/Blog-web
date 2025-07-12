cc4b2a0f1954d27b12b59f4ea3134b93d5462718
Krishna Yadav <devy3260@gmail.com>
2025-07-12 08:07:22 +0530

    hay f

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/pages/home';
+import Home from "./components/pages/home";
 import About from './components/pages/about';
 import Service from './components/pages/Service';
 import Navbar from './components/navbar';
@@ -12,10 +12,10 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="*" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/service" element={<Service />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </div>
     </HashRouter>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js or vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Blog-web/',
+  base: "/Blog-web/", // 👈 This is your GitHub repo name!
   plugins: [react()],
 });
